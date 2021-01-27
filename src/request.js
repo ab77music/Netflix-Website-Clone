f21ea64bd2ae525fec19edd4f0e9193f5cd51acc
ab77music <abelalemayehua@yahoo.com>
2021-01-27 00:43:40 -0800

    Update request.js

--- src/request.js
+++ src/request.js
@@ -10,5 +10,7 @@ const request = {
   fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
   fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
 
+
+
 };
 export default request;
